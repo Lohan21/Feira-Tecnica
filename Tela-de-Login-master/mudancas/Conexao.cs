dca1b46a72172de1f6f1de3de05d515b78490a38
Lohan Müller Sampaio <39602504+Lohan21@users.noreply.github.com>
2018-10-02 14:01:08 -0300

    Add files via upload

---
+++ Tela-de-Login-master/mudancas/Conexao.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mudancas
+{
+    class Conexao
+    {
+        private static string servername = "(localdb)\\MSSQLLocalDB";
+        private static string database = "Database";
+
+        public static SqlConnection conexao = new SqlConnection(
+            string.Format(
+                "Data Source={0};Initial Catalog={1};Integrated Security=True;Connect Timeout=5;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                servername,
+                database
+            )
+        );
+
+        public static string PegarDateTime()
+        {
+            return DateTime.Now.ToString("");
+        }
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                conexao.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        public static bool AdicionarRegistro(string tabela, Dictionary<string, string> informacoes)
+        {
+            bool erro = false;
+
+            try
+            {
+                List<string> colunas = informacoes.Keys.ToList();
+                string colunasSql = String.Join(", ", colunas);
+
+                List<string> valores = new List<string>();
+
+                foreach (KeyValuePair<string, string> informacao in informacoes)
+                {
+                    string coluna = informacao.Key;
+                    string valor = informacao.Value;
+
+                    string valorComAspasSimples = string.Format(
+                        "'{0}'",
+                        valor
+                    );
+
+                    valores.Add(valorComAspasSimples);
+                }
+
+                string valoresSql = String.Join(", ", valores);
+
+                string sql = string.Format(
+                    "INSERT INTO {0} ({1}) VALUES ({2});",
+                    tabela,
+                    colunasSql,
+                    valoresSql
+                );
+
+                SqlCommand comando = new SqlCommand(sql, conexao);
+                AbrirConexao();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao adicionar registro: " + e);
+
+                erro = true;
+            }
+            finally
+            {
+                FecharConexao();
+            }
+
+            return erro;
+        }
+
+        public static bool EditarRegistro(string tabela, int id, Dictionary<string, string> informacoes)
+        {
+            bool erro = false;
+
+            try
+            {
+                informacoes.Add("modificado", PegarDateTime());
+
+                List<string> valores = new List<string>();
+
+                foreach (KeyValuePair<string, string> informacao in informacoes)
+                {
+                    string coluna = informacao.Key;
+                    string valor = informacao.Value;
+
+                    string valorComAspasSimples = string.Format(
+                        "{0} = '{1}'",
+                        coluna,
+                        valor
+                    );
+
+                    valores.Add(valorComAspasSimples);
+                }
+
+                string valoresSql = String.Join(", ", valores);
+
+                string sql = string.Format(
+                    "UPDATE {0} SET {1} WHERE id = '{2}';",
+                    tabela,
+                    valoresSql,
+                    id
+                );
+
+                SqlCommand comando = new SqlCommand(sql, conexao);
+                AbrirConexao();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao editar registro: " + e);
+
+                erro = true;
+            }
+            finally
+            {
+                FecharConexao();
+            }
+
+            return erro;
+        }
+
+        public static bool RemoverRegistro(string tabela, int id)
+        {
+            bool erro = false;
+
+            try
+            {
+                string sql = string.Format(
+                    "UPDATE {0} SET ativo = '0' WHERE id = '{1}';",
+                    tabela,
+                    id
+                );
+                SqlCommand comando = new SqlCommand(sql, conexao);
+                AbrirConexao();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao remover o registro: " + e);
+
+                erro = true;
+            }
+            finally
+            {
+                FecharConexao();
+            }
+
+            return erro;
+        }
+
+        public static void BuscarRegistrosAtivos(string tabela, DataGridView dataGridView)
+        {
+            try
+            {
+                string sql = string.Format(
+                    "SELECT * FROM {0} WHERE ativo = '1';",
+                    tabela
+                );
+                SqlCommand comando = new SqlCommand(sql, conexao);
+                AbrirConexao();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(comando.ExecuteReader());
+                dataGridView.DataSource = dataTable;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao buscar registros: " + e);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+    }
+}
---
+++ Tela-de-Login-master/mudancas/TelaPrincipal.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace mudancas
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Location = new System.Drawing.Point(12, 36);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(71, 13);
+            this.lbUsuario.TabIndex = 0;
+            this.lbUsuario.Text = "Olá {usuario}.";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(441, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem5});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(87, 20);
+            this.toolStripMenuItem2.Text = "Funcionários";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(183, 22);
+            this.toolStripMenuItem3.Text = "Adicionar";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(183, 22);
+            this.toolStripMenuItem4.Text = "Listar";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.menuItemListarFuncionarios_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(180, 6);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(183, 22);
+            this.toolStripMenuItem5.Text = "Folha de Pagamento";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 349);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.statusStrip1.Size = new System.Drawing.Size(441, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(167, 17);
+            this.toolStripStatusLabel1.Text = "Nome da Empresa - Versão 1.0";
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 371);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lbUsuario);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TelaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TelaPrincipal";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.TelaPrincipal_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
